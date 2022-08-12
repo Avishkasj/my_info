63f7ceab528aa6bb4b5864cc00fe188d6c23312c
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-08-13 00:16:59 +0530

    done1

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -11,20 +10,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body:SafeArea(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
           child: Column(
+            mainAxisAlignment:MainAxisAlignment.center,
             children: [
-               CircleAvatar(
-                 radius: 60,
-                 backgroundImage: AssetImage('images/imag2.png'),
-               )
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/image.jpg'),
+              ),
+              Text(
+                'Avishka S ',
+                style: TextStyle(
+                  fontFamily: 'RubikDistressed',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0,
+              width: 160.0,
+              child: Divider(
+                color: Colors.white,
+
+              ),),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text(
+                        '076 2697156',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.email),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'asupunjayasooriya@gmail.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.ac_unit),
+                  title: Text('Nothing to say'),
+                ),
+              )
             ],
-          )
-        )
-      )
+          ),
+        ),
+      ),
     );
   }
 }
-
